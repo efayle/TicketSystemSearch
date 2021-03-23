3e7d21129d78e03d2f1f8e88f21a4fa8ff5aa4e2
Elena Fayle <efayle@my.wctc.edu>
2021-03-22 19:56:43 -0500

    Priority Search

--- Program.cs
+++ Program.cs
@@ -213,9 +213,30 @@ namespace TicketSystemSearch
 
                             int sum = Tickets.Count() + Tasks.Count() + Enhancements.Count();
                             Console.WriteLine("There are " + sum + " tickets with the status of " + status + ".\n");
-                            
+
                         } else if (userInput == "2") {
-                            Console.WriteLine("Priority");
+                            //priority
+
+                            Console.WriteLine("Enter priority:");
+                            string priority = Console.ReadLine();
+
+                            var Tickets = ticketFile.Systems.Where(s => s.priority.Contains(priority));
+                            foreach (System s in Tickets) {
+                                Console.WriteLine(s.Display());
+                            }
+
+                            var Tasks = taskFile.Tasks.Where(t => t.priority.Contains(priority));
+                            foreach (System s in Tasks) {
+                                Console.WriteLine(s.Display());
+                            }   
+
+                            var Enhancements = enhancementFile.Enhancements.Where(e => e.priority.Contains(priority));
+                            foreach (System s in Enhancements) {
+                                Console.WriteLine(s.Display());
+                            }
+
+                            int sum = Tickets.Count() + Tasks.Count() + Enhancements.Count();
+                            Console.WriteLine("There are " + sum + " tickets with the priority of " + priority + ".\n");
                         } else if (userInput == "3") {
                             Console.WriteLine("Submitter");
                         }
